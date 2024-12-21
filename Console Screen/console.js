158ec8d6917aa81d1f34c2693c5f88f4e7d70166
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-21 14:53:30 +0530

    First,Second

---
+++ Console Screen/console.js
@@ -0,0 +1,29 @@
+//=> Console Screen : 
+//Use : debugging and Testing 
+
+// How to Print Data in a Console Screen : 
+// console.time("Time");
+// console.log("Hello Everyone,Welcome to Javascript 😍.");
+
+
+
+// console.log([11,12,13,14,17]);
+
+// // How to Print Data in a Table Format :   Object/Array (object)
+// console.table([11,12,13,14,17]);
+
+// console.table("Zafar");  // not valid
+// console.table(1234);  // not valid
+
+// console.table({"Firtname":"Raj","Lastname" : "Shah"});
+
+
+//Showing Error  : 
+// console.error("This is an Error !!");
+
+// Show Warning : 
+// console.warn("Warning");
+
+// console.timeEnd("Time ");
+// Clear Console Screen : 
+// console.clear();
