80f7d8a4b6c42c37702fee6d116fa42782821bae
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-22 14:48:37 +0530

    Operators

---
+++ Basics/Operators.js
@@ -0,0 +1,93 @@
+// Arithmetic Op :
+    // +,-,/,*,%
+// console.log(12+13);
+// console.log(12-13);
+// console.log(12/13);
+// console.log(12*13);
+// console.log(12%13);
+
+//    -----------------------------------------------------------    //
+// Unary Op : 
+    // ++,--
+// var a =13;
+// console.log(a++); // 13 
+// console.log(++a); // 14
+// console.log(a--); // 13 
+// console.log(--a); // 12
+
+//    -----------------------------------------------------------    //
+// Relational Op : 
+    // <,>,<=,>=,==,!=        ===,!==
+
+// console.log(12<13);
+// console.log(12>13);
+// console.log(12<=13);
+// console.log(12>=13);
+// console.log(12!=13);
+// console.log(12==13);
+
+
+// check only Values 
+        // number  string
+// console.log(12=="12");  //true
+
+
+// Check Value with Type.
+// console.log("12"==="1");  //false  
+// console.log("12"!=="1");  //false  
+
+
+//    -----------------------------------------------------------    //
+// Logical Op : 
+    // && ,||, !
+
+// console.log(12<13 && 13!=14);
+
+// true = 1  false = 0
+// false= 0  numeric = true
+// console.log(10 && "Zafar");  // Zafar
+
+// 0   0   0
+// 0   1   0
+// 1   0   0
+// 1   1   1
+
+// console.log(10 || "Zafar");  // 10
+
+
+// console.log(!11);  // false
+
+
+//    -----------------------------------------------------------    //
+// Assignment Op : 
+    // =,+=,-=,/=,*=,%=
+
+// var a = "10";
+// var b = "12";
+// console.log(a);
+// console.log(b);
+
+// b+=a; // b = b+a  = 25
+// b-=a; // b = b-a  = 1
+// b*=a; // b = b*a  = 156
+// b/=a; // b = b/a  = 1.083
+// b%=a; // b = b%a  = 1
+
+
+
+
+// console.log(a);
+// console.log(b);.
+
+// console.log(0.1 + 0.2);
+//    -----------------------------------------------------------    //
+// Ternary Op : 
+    // (condition)? statement 1 : statement 2;
+
+// (12<10)? console.log("True") : console.log("False");
+//    -----------------------------------------------------------    //
+
+// String 
+// number 
+// Boolean
+
