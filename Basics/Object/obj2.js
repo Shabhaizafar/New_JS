cb732b86aac0ef837f1a42c4d14f7e7753644152
Shabhaizafar <zafarshabhai03@gmail.com>
2025-01-21 17:00:42 +0530

    Object 2

---
+++ Basics/Object/obj2.js
@@ -0,0 +1,59 @@
+// var obj = {
+//     fname : "Arin",
+//     lname : "Prajapati",
+//     Age : 21 ,
+//     Gender : "male"
+// };
+// console.log(obj);
+
+// How to Create a Empty Object.
+
+// How to access Object Properties Values.
+// How to Modify/Changed Object Properties Values.
+// How to Add Object Property.
+// How to Delete Object Property.
+
+// How to Access Object Keys : 
+
+// loops : 
+// 1. basic For Loop  : index
+// 2. for-in Loop   : Workable
+// 3. for-of Loop   : return values(array,string)     Object(Key+value)(doesn't Workable)
+// 4. foreach Loop  : return values(array,string)     Object(Key+value)(doesn't Workable)
+// 5. while Loop    : index
+// 6. do-While Loop : index
+
+// for (const key in obj) {
+    // console.log(key);
+    // console.log(key,obj[key]);
+    // console.log(key,obj.key);  // dot notation not workable
+// }
+
+
+
+// Create a Object Using User input : 
+//  key1     value1
+// fname  : name
+// lname  : name
+//  key2     value2
+
+
+
+// var key1 = "werty";
+// var key2 = "Lname";
+
+// var value1 = "Raj";
+// var value2 = "Shah";
+
+// // {
+// //     Fname : "Raj",
+// //     Lname : "Shah"
+// // }
+// var Person = {};
+
+
+// // Computed Properties :   []
+// Person[key1] = value1;
+// Person["Lname"] = value2;
+
+// console.log(Person);
