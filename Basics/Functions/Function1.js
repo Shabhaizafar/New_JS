f35d682424578493578efb0669b19ec082794038
Shabhaizafar <zafarshabhai03@gmail.com>
2025-01-25 14:33:51 +0530

    Function1

---
+++ Basics/Functions/Function1.js
@@ -0,0 +1,73 @@
+// What is Function :->
+// It is a Block of Code which is Perform Specific Task.
+
+
+// Name Function.
+/*
+// 1. without argument and without return type
+//function definition 
+function sayHello(){
+    console.log("Hello Everyone,Welcome to javascript !!!");
+}
+
+//function calling/invoked
+sayHello();
+*/
+
+/*
+// 2. with argument and without return type
+function addition(n1,n2){  // n1,n2  parameter
+    console.log("Addition is : ",n1+n2);
+}
+addition(12,13); // 12 13 argument 
+*/
+/*
+// 3. without argument and with return type
+function addition(){
+    var n1 = 12;
+    var n2 = 13;
+    return n1+n2;
+}
+// console.log("Addition is : ",addition());
+var ans  = addition();
+console.log(ans);
+
+*/
+// 4. with argument and with return type
+
+// function addition(n1,n2){
+//     return n1+n2;
+// }
+
+// // console.log("Addition",addition(12,13));
+// var ans = addition(12,13);
+// console.log(ans);
+
+// Expression Function.
+
+// Arrow Function.
+// IIFE Function.
+// Anonymous Function.
+// Generator Function.
+// Callback Function.
+// Highr-Order Function.
+// getter-Setter Function. // OOJ
+// Recursion.
+//////////////////////////////
+// 2. Create a function called `calculateArea` that takes two parameters (`width` and `height`) and returns the area of a rectangle.
+
+// 3. Write a function `isPrime` that takes a number as an argument and returns `true` if it's a prime number, otherwise `false`.
+
+// 4. Implement a function `capitalizeFirstLetter` that accepts a string and returns the same string with the first letter capitalized.
+
+// 5. Define a function `sumArray` that takes an array of numbers as an argument and returns the sum of all the numbers.
+
+// 6. Create a function `reverseString` that reverses a given string without rtn.
+
+// 7. Write a function `countVowels` that counts the number of vowels (a, e, i, o, u) in a given string and returns that count.
+
+// 8. Implement a function `filterEvenNumbers` that takes an array of numbers and returns a new array with only the even numbers .
+
+// 9. Define a function `mergeArrays` that takes two arrays as arguments and returns a new array that merges both arrays.
+
+// 10. Create a function `calculateFactorial` that computes the factorial of a number (`n`) without rtn.
