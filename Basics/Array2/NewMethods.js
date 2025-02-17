5a96ba9f1d5161befa190d5bf272552bed869e3a
Shabhaizafar <zafarshabhai03@gmail.com>
2025-02-17 16:03:41 +0530

    Array2

---
+++ Basics/Array2/NewMethods.js
@@ -0,0 +1,91 @@
+// primitive  vs reference Datatype : 
+// Number,String,Boolean,Object,BigInt,Function etc
+
+// primitive Datatype : 
+// Number,String,Boolean,BigInt
+
+// Reference Data Type  : 
+// Object=> object,array,null ,function
+
+
+// var n1=12;
+// var n2 = n1;
+
+// console.log("Before  : ");
+// console.log("Value of N1: ",n1);
+// console.log("Value of N2: ",n2);
+// // n1++;
+// n2++;
+// console.log("After  : ");
+// console.log("Value of N1: ",n1);
+// console.log("Value of N2: ",n2);
+
+
+// var Arr = [11,12,13];
+// var Arr2 = Arr;
+
+// console.log("Before  : ");
+// console.log("Value of Arr1: ",Arr);
+// console.log("Value of Arr2: ",Arr2);
+
+// Arr.push(100);
+
+// console.log("After  : ");
+// console.log("Value of Arr1: ",Arr);
+// console.log("Value of Arr2: ",Arr2);
+
+// Array's Method's : 
+
+
+// const arr = [12,14,16,20,35];
+// console.log("Array is  : ",arr);
+// // foreach 
+// var ans = arr.forEach((value,index,itSelf)=>{
+//     return value;
+// });
+// console.log(ans);
+
+// // map  
+// var ans2= arr.map((value,index,it)=>{
+//     if(value>=20){
+//         return value;
+//     }
+// });
+// console.log(ans2);
+
+
+// // filter
+// var ans3 = arr.filter((value,i,a)=>{
+//     if(value>=20){
+//         return value;
+//     }
+// });
+// console.log(ans3);
+
+
+// fill
+// sort 
+// every
+// some 
+// find
+// reduce
+// splice
+
+
+/*
+
+map : 
+1.Given an array of objects with name and age, return a new array containing only the names.
+2.Multiply each number in an array by 10 and return the new array.
+
+foreach : 
+1.Given an array of numbers, use forEach() to print each number multiplied by 2.
+2.Modify an array of numbers by adding 5 to each number using forEach().
+
+
+
+Filter: 
+1.Given an array of numbers, return a new array containing only even numbers.
+2Filter out all falsy values (false, 0, "", null, undefined, NaN) from an array.
+3.Given an array of students with scores, return only the students who scored above 80.
+*/ 
