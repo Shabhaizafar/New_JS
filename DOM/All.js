2f1e63f0e1fadc206a0536e76d0fe4e1b1381673
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-14 16:44:39 +0530

    Access,modify,add style

---
+++ DOM/All.js
@@ -0,0 +1,24 @@
+// How to access Element Text Using JS : 
+// innerHTML : 
+// console.log(document.querySelector('div').innerHTML);
+// innerText : 
+// console.log(document.querySelector('div').innerText);
+// textContent : 
+// console.log(document.querySelector('div').textContent);
+
+
+
+// How to Modifiy Element Text Using JS : 
+
+// document.querySelector('h1').innerHTML = "Royal";
+
+// document.querySelector('span').innerText = "NewContent";
+
+// document.querySelector('span').textContent = "NewContent";
+
+
+// How to Add/Change Style Using a JS : 
+// document.querySelector('h1').style.color = "red";
+// document.querySelector('h1').style.fontSize = "70px";
+
+// document.querySelector('span').style.fontSize = "24px";
